ba985895b15b8de78cd2859e552785d4e3b37674
Vladimir Kholiavko <thiswolfram@gmail.com>
2023-02-01 16:17:53 +0300

    fixed names

--- src/components/clientBlock/index.js
+++ src/components/clientBlock/index.js
@@ -14,12 +14,12 @@ class ClientBlock extends Component {
 
     componentDidMount() {
         const {client} = this.props;
-        /*if (client.avatar?.type && client.avatar?.filename) {
+        if (client.avatar?.type && client.avatar?.filename) {
             handleGetAvatarFile(client.avatar.type, client.avatar.filename)
               .then((url) => {
                   this.setState({avatarImg: url})
               })
-        }*/
+        }
     }
 
     render() {
@@ -42,14 +42,14 @@ class ClientBlock extends Component {
                     <div className={'rating-wrapper'}>
                         <Rating
                             name="read-only"
-                            /*value={client.rating}*/
+                            value={client.rating}
                             readOnly
                             precision={0.1}
                             icon={<i className="fas fa-star filled-icon"/>}
                             emptyIcon={<i className="fas fa-star empty-icon"/>}
                         />
-                        <span className={'number-of-rating'}>{/*client.rating*/}</span>
-                        <span className={'number-of-votes'}>({/*client.numberOfReviews*/})</span>
+                        <span className={'number-of-rating'}>{client.rating}</span>
+                        <span className={'number-of-votes'}>({client.numberOfReviews})</span>
                     </div>
                 </div>
             </div>
